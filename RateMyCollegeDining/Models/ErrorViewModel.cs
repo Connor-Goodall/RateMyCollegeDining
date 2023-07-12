c36677deeb35b9069769472c4626e3704c984711
Connor Goodall <burningshock83@gmail.com>
2023-07-12 16:37:13 -0400

    First commit for this website

---
+++ RateMyCollegeDining/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RateMyCollegeDining.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
